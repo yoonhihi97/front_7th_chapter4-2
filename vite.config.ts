e25ff50d83d88c8162c0e35541e42857d81012fc
윤지 <85012406+yoonhihi97@users.noreply.github.com>
2025-12-22 21:38:06 +0900

    chore: @ path alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   }),
   defineTestConfig({
     test: {
